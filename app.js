12b685e09c98042e52c2174b99330b94975bdb37
Radosław Urban <urban_radoslaw@o2.pl>
2021-10-11 18:17:09 +0200

    feature-min-max-value

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const _ = require("lodash");
 
 
 let tablica = [1, 2, 3, 4, 5];
-let avg = _.mean(tablica);
 
-console.log(avg);
+console.log(_.mean(tablica));
+console.log(_.min(tablica));
+console.log(_.max(tablica));
