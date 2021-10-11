2c3e1978e37696f86d3cdaca6c3408c68a9bf32c
Radosław Urban <urban_radoslaw@o2.pl>
2021-10-11 19:03:01 +0200

    first commit new repo

--- app.js
+++ app.js
@@ -1,8 +1,42 @@
 const _ = require("lodash");
-
+const user = {
+    name: 'Imie',
+    surname: 'Nazwisko',
+    allGrades: [{
+            subjectName: 'Name1',
+            grades: [5, 4, 3, 2, 1],
+            weight: 3
+        },
+        {
+            subjectName: 'Name2',
+            grades: [3, 3.5, 2],
+            weight: 1
+        },
+        {
+            subjectName: 'Name3',
+            grades: [4, 3, 3.5],
+            weight: 5
+        }
+    ]
+}
 
 let tablica = [1, 2, 3, 4, 5];
 
 console.log(_.mean(tablica));
 console.log(_.min(tablica));
 console.log(_.max(tablica));
+
+printUser(user);
+
+function printUser(user) {
+
+    let avgUser = _.meanBy(user.allGrades, (p) => _.reduce(p.grades, function (sum, n) {
+        return sum + (n * p.weight) / (p.grades.length * p.weight);
+    }, 0))
+
+    console.log(user.name + ' ' + user.surname + ': ' + avgUser);
+
+    console.log(
+
+    );
+}
